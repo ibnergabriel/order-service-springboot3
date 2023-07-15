02380e5a1d49cc4c41c58df27bba74fe22a01171
ibnergabriel <ibner68@gmail.com>
2023-07-14 22:41:29 -0300

    Project Created

---
+++ src/main/java/br/com/gabrielibner/orderservice/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielibner.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
