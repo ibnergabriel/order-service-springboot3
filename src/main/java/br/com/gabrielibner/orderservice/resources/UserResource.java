844ecb536751b725dc9c10b2646bfa3bc4083321
ibnergabriel <ibner68@gmail.com>
2023-07-16 23:36:15 -0300

    feat: Service layer, component registration

--- src/main/java/br/com/gabrielibner/orderservice/resources/UserResource.java
+++ src/main/java/br/com/gabrielibner/orderservice/resources/UserResource.java
@@ -1,21 +1,34 @@
 package br.com.gabrielibner.orderservice.resources;
 
+import br.com.gabrielibner.orderservice.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gabrielibner.orderservice.entities.User;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+    private UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(
-            "Gabriel", "ibner@gmail.com", "21999261398", "1234");
-        return ResponseEntity.ok().body(user); 
-    
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/br/com/gabrielibner/orderservice/services/UserService.java
@@ -0,0 +1,26 @@
+package br.com.gabrielibner.orderservice.services;
+
+import br.com.gabrielibner.orderservice.entities.User;
+import br.com.gabrielibner.orderservice.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.get();
+    }
+}
